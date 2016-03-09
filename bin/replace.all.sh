8188bc62e98290c7ea2b1aaaa9a1fca43c56c9d0
chfw <wangc_2011@hotmail.com>
2016-03-09 14:09:33 +0000

    added simple usage and fix a bug in quoting spaces for grep

--- bin/replace.all.sh
+++ bin/replace.all.sh
@@ -3,8 +3,18 @@
 #  Replace text with something else in a given directory
 #
 
+function usage(){
+    echo "replace.all.sh what_be_replaced replace_with_what where"
+    exit 1
+}
+
+if [ $# != 3 ]
+then
+  usage
+fi
+
 what=$1
 to=$2
 where=$3
 
-grep $what $where -Rl | xargs sed -i -e "s/$what/$to/g"
+grep "$what" $where -Rl | xargs sed -i -e "s/$what/$to/g"
